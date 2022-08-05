962eb83b4a6fd40bd5b1e3a1f05bde4eeeda64cd
zyy <zyy@zyydeMacBook-Pro.local>
2022-08-05 20:38:44 +0800

    change cart

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -15,30 +15,70 @@ const addCartItem = (cartItems, productToAdd) => {
   return [ ...cartItems, { ...productToAdd, quantity: 1 }];
 }
 
+const removeCarItem = (cartItems, cartItemToRemove) => {
+  //找到要减少的商品
+  const existingCartItem = cartItems.find((cartItem) => cartItem.id === cartItemToRemove.id);
+
+  //如果数量为1，从购物车中删除该商品
+  if (existingCartItem.quantity === 1) {
+    console.log("count 1")
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+  }
+  //否则数量减1
+  return cartItems.map((cartItem) => 
+    cartItem.id === cartItemToRemove.id 
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+}
+
+const clearCarItem = (cartItems, cartItemToClear) => {
+
+  return cartItems.filter(cartItem => cartItem.id !== cartItemToClear.id);
+  
+
+}
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
   cartItems: [],
-  //我们不想直接提供set方法改变cartItems，而是另外提供一个方法
   addItemToCart: () => {},
-  cartCount: 0
+  removeItemToCart: ()=> {},
+  clearItemFromCart: () => {},
+  cartCount: 0,
+  cartTotal: 0
 });
 
 export const CartProvider = ({children}) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
   const [cartCount, setCartCount] = useState(0);
+  const [cartTotal, setCartTotal] = useState(0);
+
 
   useEffect(() => {
     const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
     setCartCount(newCartCount);
   }, [cartItems])
 
+  useEffect(() => {
+    const newCartTotal = cartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price, 0)
+    setCartTotal(newCartTotal);
+  }, [cartItems])
+
   const addItemToCart = (productToAdd) => {
     setCartItems(addCartItem(cartItems, productToAdd));
   }
 
-  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount };
+  const removeItemToCart = (cartItemToRemove) => {
+    setCartItems(removeCarItem(cartItems, cartItemToRemove));
+  }
+  const clearItemFromCart = (cartItemToClear) => {
+    setCartItems(clearCarItem(cartItems, cartItemToClear));
+  }
+
+  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount, removeItemToCart, clearItemFromCart, cartTotal };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
